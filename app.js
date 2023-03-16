94617f862e75767e9515e0c28240050bbb3ded26
hafizakbar19 <hafizakbar19@gmail.com>
2023-03-16 16:54:30 +0500

    general practice

---
+++ app.js
@@ -0,0 +1,14 @@
+let myVar = prompt("Enter your charecter: ");
+switch(myVar){
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+    document.write("this is a vowel");
+    break;
+    default:
+        document.write("This is a consonant");
+
+
+}
