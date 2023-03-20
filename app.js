0fb7b6a38e6df806b78787236a39fb4f9e0a8767
hafizakbar19 <hafizakbar19@gmail.com>
2023-03-20 16:07:35 +0500

    practice

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-let myVar = prompt("Enter your charecter: ");
-switch(myVar){
-    case "a":
-    case "e":
-    case "i":
-    case "o":
-    case "u":
-        document.write("this is a vowel");
-        break;
-        default:
-        document.write("This is a consonant");
+// let myVar = prompt("Enter your charecter: ");
+// switch(myVar){
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         document.write("this is a vowel");
+//         break;
+//         default:
+//         document.write("This is a consonant");
 
 
-}
-
+// }
+let weight = 350;
+let time = 5;
+if (weight > 300 && time < 6) {
+     console.log("Come to our tryout!");
+     }
+     else {
+     console.log("Come to our cookout!");
+     }
+    
 
