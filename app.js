ba5ff64f99f7a19515377c5d51cecd4dbaa15d50
hafizakbar19 <hafizakbar19@gmail.com>
2023-03-20 17:19:53 +0500

    practice

--- app.js
+++ app.js
@@ -12,13 +12,17 @@
 
 
 // }
-let weight = 350;
-let time = 5;
-if (weight > 300 && time < 6) {
-     console.log("Come to our tryout!");
-     }
-     else {
-     console.log("Come to our cookout!");
-     }
+// let weight = 350;
+// let time = 5;
+// if (weight > 300 && time < 6) {
+//      console.log("Come to our tryout!");
+//      }
+//      else {
+//      console.log("Come to our cookout!");
+//      }
     
-
+//creating and epmty array and then adding the value to it
+let boys = [];
+boys[0] = "Akbar";
+boys[1] = "Noor";
+console.log(boys);
